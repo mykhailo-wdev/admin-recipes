fea55344e7ff14d32365868af22aa540edbb1ef1
Mike Mike <mike_insider@proton.me>
2024-12-03 20:35:10 +0200

    Project sceleton

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="root">
+        <div class="menu"></div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/index.scss' as *;
+
+</style>
