aa436a2086a1132f1df0a0f931e4a21e563fda3f
Mike Mike <mike_insider@proton.me>
2024-12-03 21:26:12 +0200

    Organized first part

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,14 +1,52 @@
 <template>
     <div class="root">
-        <div class="menu"></div>
+        <nav class="menuInner">
+            <AppMenu/>
+        </nav>
+        <main class="main">
+            <header class="header">
+                <div class="title">
+                    <slot name="title"></slot>
+                </div>
+                <div class="controls">
+                    <slot name="controls"></slot>
+                </div>
+            </header>
+        </main>
     </div>
 </template>
 
 <script setup>
-
+import AppMenu from '@/components/AppMenu.vue';
 </script>
 
 <style lang="scss" scoped>
 @use '@/assets/styles/index.scss' as *;
+
+.menuInner {
+    width: 70px;
+}
+
+.root {
+    display: flex;
+}
+
+.main {
+    flex: 1;
+    padding: 20px 16px;
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.title {
+    color: $violet;
+    font-size: 28px;
+    font-weight: 700;
+}
+
 
 </style>
