c0f9c3de4a2138ecb2685338a93d2f6e56d85731
Mike Mike <mike_insider@proton.me>
2025-01-16 21:26:40 +0200

    Added functions create and update recipe

--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -7,14 +7,23 @@
             <AppButton text="Добавити категорію"/>
         </template>
         <template #inner>
-            
+            <el-table :data="categories" style="width: 100%">
+                <el-table-column prop="strCategory" label="Category" />
+            </el-table>
         </template>
     </AppLayout>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
+import { useRootStore } from '@/stores/root';
 import AppLayout from '@/layouts/AppLayout.vue';
 import AppButton from '@/components/AppButton.vue';
+
+
+const rootStore = useRootStore();
+
+
+const categories = computed(() => rootStore.categories)
 
 </script>
